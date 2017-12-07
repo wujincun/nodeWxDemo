c458f551d4be25d6fd7d2c65170544d8cdaa093e
wujincun <1769453779@qq.com>
2017-12-07 18:13:15 +0800

    微信回复处理  未完

--- wechat/wechat/g.js
+++ wechat/wechat/g.js
@@ -1,13 +1,13 @@
 'use strict'
 var sha1 = require('sha1');
-const xmlParser = require('koa-xml-body')
 var WechatApi = require('./wechatApi.js');
+var getRawBody = require("raw-body");
 
 /*这个中间件是处理事件，推送数据等，用来返回信息*/
 module.exports = function(opts) {
 	//var Wechat = new WechatApi(opts);
 	return async(ctx, next) => {
-		await next();
+		//await next(); //什么意思,指Wechat？
 		var query = ctx.request.query;
 		var method = ctx.request.method;
 		var response = ctx.response;
@@ -18,10 +18,10 @@ module.exports = function(opts) {
 		var echostr = query.echostr;
 		var str = [token, timestamp, nonce].sort().join('');
 		var sha = sha1(str);
-		console.log(ctx)
 		if (method == 'GET') {
 			if (sha === signature) {
 				response.body = echostr + '';
+				console.log('连接成功')
 			} else {
 				response.body = 'wrong'
 			}
@@ -30,16 +30,8 @@ module.exports = function(opts) {
 				response.body = 'wrong'
 				return false
 			}
-			xmlParser({
-				encoding: 'utf8', // lib will detect it from `content-type` 
-				xmlOptions: {
-					explicitArray: false
-				},
-				onerror: (err, ctx) => {
-					ctx.throw(err.status, err.message);
-				}
-			});
-
+			console.log(ctx.request.body.xml)
+			response.body = 'post'
 		}
 	}
 }
---
+++ wechat/wechat/util.js
@@ -0,0 +1,12 @@
+'use strict'
+
+function formatMessage(result) {
+	var message = {};
+	if (typeof result === 'object') {
+		var keys = object.keys(result);
+		for (var i = 0; i < keys.length; i++) {
+			var item = result[keys[i]];
+			var key = keys[i];
+		}
+	}
+}
